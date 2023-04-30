6dff0d30960514ccd68f063c48c9d7555511312a
Pooja <pooja.it25@gmail.com>
2023-04-30 13:33:50 +0530

    Square root of given number

---
+++ 26april.js
@@ -0,0 +1,16 @@
+// Program to Find the Square Root of given Number
+// Method 1
+// const prompt = require("prompt-sync")();
+// const number = prompt('Enter the number: ');
+
+// const result = Math.sqrt(number);
+// console.log(`The square root of ${number} is ${result}`);
+
+
+// Method 2
+const number1 = 9;
+const number2 = 100;
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
