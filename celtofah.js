e1a2e00155d82e6a0397bb714babb3a41570bb35
Pooja <pooja.it25@gmail.com>
2023-05-14 17:38:17 +0530

    celsius to fahrenheit

---
+++ celtofah.js
@@ -0,0 +1,17 @@
+// Program to Convert Celsius to Fahrenheit
+
+// let celsiusTemperature = 55;
+// let b = celsiusTemperature * 1.8 + 32;
+// console.log(b);
+
+function password(pass) {
+    for (i = 0; i < pass.length; i++) {
+     }
+    }
+    function name(pname) {
+    console.log("The value of i is "+i);
+    for (i = 0; i < pname.length; i++) {
+     }
+    }
+    password("54321");
+    name("John");
